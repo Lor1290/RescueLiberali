cfc6020c744800b13cb35743d4132a6d0e985096
PoliCoding <lor12900000@gmail.com>
2026-01-12 23:14:40 +0100

    main fix

--- Code/src/main.py
+++ Code/src/main.py
@@ -1,5 +1,6 @@
-from controller import DistanceSensor, Emitter, Robot, Camera, Liadar, GPS 
-
+from controller import DistanceSensor, Emitter, Robot, Camera, Lidar, InertialUnit, GPS
+import numpy as np
+import math
 
 # **************** #
 # *** CONSTANT *** #
@@ -19,11 +20,11 @@ lWheel = robot.getDevice("wheel2 motor")
 rWheel.setPosition(float("inf"))
 lWheel.setPosition(float("inf"))
 
-rWhell.setVelocity(0)
+rWheel.setVelocity(0)
 lWheel.setVelocity(0)
 
-rCamera = robot.getDevice("camera1")
-lCamera = robot.getDevice("camera2")
+rCamera = robot.getDevice("leftcamera")
+lCamera = robot.getDevice("rightcamera")
 
 rCamera.enable(TIMESTEP)
 lCamera.enable(TIMESTEP)
@@ -32,7 +33,6 @@ sColor = robot.getDevice("colour_sensor")
 sColor.enable(TIMESTEP)
 
 receiver = robot.getDevice("receiver")
-emitter = robot.getDevice("emitter")
 receiver.enable(TIMESTEP)
 
 gps = robot.getDevice("gps")
@@ -41,16 +41,32 @@ gps.enable(TIMESTEP)
 lidar = robot.getDevice("lidar")
 lidar.enable(TIMESTEP)
 
+iu = robot.getDevice("inertial_unit")
+iu.enable(TIMESTEP)
+
+emitter = robot.getDevice("emitter")
 
 # **************** #
 # *** MOVEMENT *** #
 # **************** #
 
-def driveTo(dx: int, sx: int):
-        rWheel.setVelocity(dx)
-        lWheel.setVelocity(sx)
+def goFwd(dx = VELOCITY, sx = VELOCITY):
+    rWheel.setVelocity(dx)
+    lWheel.setVelocity(sx)
 
-def stop(ms):
+def goLeft(dx = VELOCITY, sx = VELOCITY):
+    rWheel.setVelocity(dx)
+    lWheel.setVelocity(-sx)
+
+def goRight(dx = VELOCITY, sx = VELOCITY):
+    rWheel.setVelocity(-dx)
+    lWheel.setVelocity(sx)
+
+def goBack(dx = VELOCITY, sx = VELOCITY):
+    rWheel.setVelocity(-dx)
+    lWheel.setVelocity(-sx)
+
+def stop(ms = 2000):
     rWheel.setVelocity(0)
     lWheel.setVelocity(0)
     delay(ms)
@@ -59,7 +75,7 @@ def delay(ms):
     intial_time = robot.getTime()
     while robot.step(TIMESTEP) != -1:
         if (robot.getTime() - intial_time) * 1000 >= ms:
-            break
+            break   
 
 
 # ************** #
@@ -79,11 +95,26 @@ def getPosition():
     y = position[2] * 100
     return x, y
 
+def getIU():
+    yaw = np.round(iu.getRollPitchYaw()[2] * (180/math.pi), 1)
+    return yaw
+
+
+def Test():
+    yaw = getIU()
+    goLeft()
+
+    while True:
+        yaw = getIU()
+        print(yaw)
+
+
 # ************ #
 # *** MAIN *** #
 # ************ #
 def main():
-    pass
+    while robot.step(TIMESTEP) != -1:
+        Test()
 
 if __name__ == "__main__":
     main()
